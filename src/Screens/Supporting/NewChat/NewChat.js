3f2d602304c3ca6008c96e3b7b9af040515e4aa0
namanicebed <naman.icebed@gmail.com>
2020-04-17 02:08:09 +0530

    added contacts screen

--- src/Screens/Supporting/NewChat/NewChat.js
+++ src/Screens/Supporting/NewChat/NewChat.js
@@ -1,14 +1,53 @@
 import React, {Component} from 'react';
-import {Text, View, TextInput, PermissionsAndroid} from 'react-native';
+import {
+  Text,
+  View,
+  TextInput,
+  PermissionsAndroid,
+  Platform,
+  SectionList,
+} from 'react-native';
 import SafeAreaView from 'react-native-safe-area-view';
 import {useTheme} from '@react-navigation/native';
 import Contacts from 'react-native-contacts';
+import _ from 'lodash';
+import {contactsArrayFilter} from '../../../Utils/ContactArrayFilter';
 
 import TopBar from './components/TopBar';
 
+const DATA = [
+  {
+    title: 'Main dishes',
+    data: ['Pizza', 'Burger', 'Risotto'],
+  },
+  {
+    title: 'Sides',
+    data: ['French Fries', 'Onion Rings', 'Fried Shrimps'],
+  },
+  {
+    title: 'Drinks',
+    data: ['Water', 'Coke', 'Beer'],
+  },
+  {
+    title: 'Desserts',
+    data: ['Cheese Cake', 'Ice Cream'],
+  },
+];
+
+var contactArr = [];
+
+const CONTACT_DATA = [];
+
 class NewChat extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      contactArray: [],
+    };
+  }
+
   componentDidMount() {
-    this.requestContactPermission()
+    Platform.OS == 'android' ? this.requestContactPermission() : null;
   }
   requestContactPermission = async () => {
     try {
@@ -24,14 +63,7 @@ class NewChat extends Component {
         },
       );
       if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-        Contacts.getAll((err, contacts) => {
-          if (err === 'denied') {
-            console.log(denied);
-          } else {
-            // contacts returned in Array
-            console.log(contacts);
-          }
-        });
+        this._getContacts();
       } else {
         console.log('Camera permission denied');
       }
@@ -40,7 +72,36 @@ class NewChat extends Component {
     }
   };
 
+  _getContacts = () => {
+    Contacts.getAll((err, contacts) => {
+      if (err === 'denied') {
+        console.log(denied);
+      } else {
+        // contacts returned in Array
+        // console.log(contacts);
+        contacts.forEach((contact) => {
+          const contactObj = {
+            name: contact.displayName,
+            number: _.isEmpty(contact.phoneNumbers)
+              ? null
+              : contact.phoneNumbers[0].number,
+          };
+          contactArr.push(contactObj);
+        });
+        let arr = contactsArrayFilter(contactArr);
+        this.setState({
+          contactArray: arr,
+        });
+      }
+    });
+  };
+
+  componentWillUnmount() {
+    contactArr = [];
+  }
+
   render() {
+    console.log(this.state.contactArray);
     return (
       <SafeAreaView
         style={{
@@ -51,9 +112,116 @@ class NewChat extends Component {
           colorScheme={this.props.colorScheme}
           navigation={this.props.navigation}
         />
+        <SectionList
+          sections={this.state.contactArray}
+          keyExtractor={(item, index) => item + index}
+          renderItem={({item}) => {
+            console.log(item);
+            return <Item title={item.name} phone={item.number} />;
+          }}
+          renderSectionHeader={({section: {title}}) => (
+            <View
+              style={{
+                backgroundColor: this.props.colorScheme.secondary,
+                justifyContent: 'center',
+                height: 30,
+              }}>
+              <Text
+                style={{
+                  fontSize: 18,
+                  marginLeft: 40,
+                  color: 'grey',
+                }}>
+                {title}
+              </Text>
+            </View>
+          )}
+        />
       </SafeAreaView>
     );
   }
+}
+
+function Avatar({name}) {
+  var nameArr = name.split(' ');
+  if (nameArr.length > 1)
+    return (
+      <View
+        style={{
+          height: 50,
+          width: 50,
+          backgroundColor: stringToHslColor(name, 60, 70),
+          borderRadius: 50 / 2,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+        <Text
+          style={{fontSize: 18, fontFamily: 'OpenSans-Regular', color: '#000'}}>
+          {nameArr[0][0].toUpperCase()}
+          {nameArr[1][0].toUpperCase()}
+        </Text>
+      </View>
+    );
+  else
+    return (
+      <View
+        style={{
+          height: 50,
+          width: 50,
+          backgroundColor: stringToHslColor(name, 90, 70),
+          borderRadius: 50 / 2,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+        <Text
+          style={{fontSize: 18, fontFamily: 'OpenSans-Regular', color: '#000'}}>
+          {name[0].toUpperCase()}
+        </Text>
+      </View>
+    );
+}
+
+function stringToHslColor(str, s, l) {
+  var hash = 0;
+  for (var i = 0; i < str.length; i++) {
+    hash = str.charCodeAt(i) + ((hash << 5) - hash);
+  }
+
+  var h = hash % 360;
+  return 'hsl(' + h + ', ' + s + '%, ' + l + '%)';
+}
+
+function Item({title, phone}) {
+  return (
+    <View
+      style={{
+        // backgroundColor: '#f9c2ff',
+        marginHorizontal: 20,
+        marginVertical: 15,
+        alignItems: 'center',
+        flexDirection: 'row',
+      }}>
+      <Avatar name={title} />
+      <View style={{justifyContent: 'space-between', marginLeft: 20}}>
+        <Text
+          style={{
+            color: '#fff',
+            fontSize: 16,
+            fontFamily: 'OpenSans-Regular',
+          }}>
+          {title}
+        </Text>
+        <Text
+          style={{
+            color: 'grey',
+            fontSize: 14,
+            fontFamily: 'OpenSans-Regular',
+          }}>
+          {phone}
+        </Text>
+      </View>
+    </View>
+  );
 }
 
 export default function NewChatScreen(props) {
---
+++ src/Utils/ContactArrayFilter.js
@@ -0,0 +1,110 @@
+export function contactsArrayFilter(contacts) {
+  const CONTACT_DATA = [];
+  CONTACT_DATA.push(
+    {
+      title: 'A',
+      data: contacts.filter((contact) => contact.name[0] == 'A'),
+    },
+    {
+      title: 'B',
+      data: contacts.filter((contact) => contact.name[0] == 'B'),
+    },
+    {
+      title: 'C',
+      data: contacts.filter((contact) => contact.name[0] == 'C'),
+    },
+    {
+      title: 'D',
+      data: contacts.filter((contact) => contact.name[0] == 'D'),
+    },
+    {
+      title: 'E',
+      data: contacts.filter((contact) => contact.name[0] == 'E'),
+    },
+    {
+      title: 'F',
+      data: contacts.filter((contact) => contact.name[0] == 'F'),
+    },
+    {
+      title: 'G',
+      data: contacts.filter((contact) => contact.name[0] == 'G'),
+    },
+    {
+      title: 'H',
+      data: contacts.filter((contact) => contact.name[0] == 'H'),
+    },
+    {
+      title: 'I',
+      data: contacts.filter((contact) => contact.name[0] == 'I'),
+    },
+    {
+      title: 'J',
+      data: contacts.filter((contact) => contact.name[0] == 'J'),
+    },
+    {
+      title: 'K',
+      data: contacts.filter((contact) => contact.name[0] == 'K'),
+    },
+    {
+      title: 'L',
+      data: contacts.filter((contact) => contact.name[0] == 'L'),
+    },
+    {
+      title: 'M',
+      data: contacts.filter((contact) => contact.name[0] == 'M'),
+    },
+    {
+      title: 'N',
+      data: contacts.filter((contact) => contact.name[0] == 'N'),
+    },
+    {
+      title: 'O',
+      data: contacts.filter((contact) => contact.name[0] == 'O'),
+    },
+    {
+      title: 'P',
+      data: contacts.filter((contact) => contact.name[0] == 'P'),
+    },
+    {
+      title: 'Q',
+      data: contacts.filter((contact) => contact.name[0] == 'Q'),
+    },
+    {
+      title: 'R',
+      data: contacts.filter((contact) => contact.name[0] == 'R'),
+    },
+    {
+      title: 'S',
+      data: contacts.filter((contact) => contact.name[0] == 'S'),
+    },
+    {
+      title: 'T',
+      data: contacts.filter((contact) => contact.name[0] == 'T'),
+    },
+    {
+      title: 'U',
+      data: contacts.filter((contact) => contact.name[0] == 'U'),
+    },
+    {
+      title: 'V',
+      data: contacts.filter((contact) => contact.name[0] == 'V'),
+    },
+    {
+      title: 'W',
+      data: contacts.filter((contact) => contact.name[0] == 'W'),
+    },
+    {
+      title: 'X',
+      data: contacts.filter((contact) => contact.name[0] == 'X'),
+    },
+    {
+      title: 'Y',
+      data: contacts.filter((contact) => contact.name[0] == 'Y'),
+    },
+    {
+      title: 'Z',
+      data: contacts.filter((contact) => contact.name[0] == 'Z'),
+    },
+  );
+  return CONTACT_DATA;
+}
